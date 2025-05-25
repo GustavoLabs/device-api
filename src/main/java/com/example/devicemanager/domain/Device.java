7fc832bf28c5cd051caa821c7fcb6c6c443cf4c9
Gustavo <gusta.rgouvea@gmail.com>
2025-05-25 10:55:19 -0300

    [Device-Ap] Updated gitignore file

---
+++ src/main/java/com/example/devicemanager/domain/Device.java
@@ -0,0 +1,31 @@
+package com.example.devicemanager.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "devices")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Device implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String brand;
+
+    @Enumerated(EnumType.STRING)
+    private State state;
+
+    @Column(name = "creation_time", updatable = false, nullable = false)
+    private Instant creationTime;
+}
